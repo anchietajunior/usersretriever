d764e787b27430e062b1f897baacaad0a8c8fa3f
anchietajunior <santosjr87@gmail.com>
2023-03-21 21:48:33 -0300

    Update search interactor

--- app/services/users/interactors/search_by_name_or_email.rb
+++ app/services/users/interactors/search_by_name_or_email.rb
@@ -13,19 +13,43 @@ module Users
 
       private
 
-      def build_search_params
-        return {} if params[:name_or_email].blank?
+      def build_name_or_email_param
+        return {} if params["name_or_email"].blank?
 
-        users = User.arel_table
-        users[:name].matches("%#{params[:name_or_email]}%").or(
-          users[:email].matches("%#{params[:name_or_email]}%")
+        users_table[:name].matches("%#{params['name_or_email']}%").or(
+          users_table[:email].matches("%#{params['name_or_email']}%")
         )
       end
 
+      def build_gender_param
+        return {} if params["gender"].blank?
+
+        users_table[:gender].eq(params["gender"])
+      end
+
+      def build_age_param
+        return {} if params["age"].blank?
+
+        start_date = Date.new(2000, 1, 1)
+        end_date = Date.new(2000, 12, 31)
+
+        if params["age"] == "before"
+          users_table[:birthdate].lteq(start_date)
+        else
+          users_table[:birthdate].gteq(end_date)
+        end
+      end
+
+      def users_table
+        @users_table ||= User.arel_table
+      end
+
       def users
-        User.select(:id, :name, :email, :gender, :birthdate)
-          .where(build_search_params)
-          .order(name: :asc)
+        User.select(:id, :name, :email, :gender, :birthdate, :created_at)
+          .where(build_name_or_email_param)
+          .where(build_gender_param)
+          .where(build_age_param)
+          .order(created_at: :desc)
           .distinct
       end
     end
